e244f08d11eb6f3d0f3803ab5fba69718f9cdf09
hrsi88 <hrsi88@gmail.com>
2018-06-18 20:11:08 +0300

    Initial commit.

---
+++ LiveColorArduinoSide/LiveColorArduinoSide.ino
@@ -0,0 +1,54 @@
+
+int redPin = 11;
+int bluePin = 10;
+int greenPin = 9;
+
+String serialData;
+
+void setup()
+{
+	pinMode(redPin, OUTPUT);
+	pinMode(bluePin, OUTPUT);
+	pinMode(greenPin, OUTPUT);
+	digitalWrite(bluePin, HIGH);
+	digitalWrite(redPin, HIGH);
+	digitalWrite(greenPin, HIGH);
+	Serial.begin(9600);
+	Serial.setTimeout(1);
+}
+
+void loop()
+{}
+
+void color (unsigned char red, unsigned char green, unsigned char blue)// the color generating function
+{
+  analogWrite(redPin, 255 - red);   // PWM signal output
+  analogWrite(greenPin, 255 - green); // PWM signal output
+  analogWrite(bluePin, 255 - blue); // PWM signal output
+}
+
+void serialEvent()
+{
+	serialData = Serial.readString(); // example: R65G120B4
+	color(getRed(serialData), getGreen(serialData), getBlue(serialData));
+}
+
+int getRed(String data)
+{
+	data.remove(data.indexOf("G"));
+	data.remove(0, 1);
+	return data.toInt();
+}
+
+int getGreen(String data)
+{
+	data.remove(data.indexOf("B"));
+	data.remove(0, data.indexOf("G") + 1);
+	return data.toInt();
+}
+
+int getBlue(String data)
+{
+	data.remove(0, data.indexOf("B") + 1);
+	return data.toInt();
+}
---
+++ Src/LiveColor.java
@@ -0,0 +1,138 @@
+// LiveColor
+// Yarden Shoham 2018
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.util.Hashtable;
+import gnu.io.*;
+import java.io.*;
+
+/**
+* Frame and main. Contains 3 sliders (RGB).
+*/
+public class LiveColor extends JFrame implements ChangeListener
+{
+	private JSlider redSlider, greenSlider, blueSlider; // the color sliders
+	private JLabel redLabel, greenLabel, blueLabel; // the label
+	private OutputStream port; // the port through which we'll comunicate with the arduino
+
+	/**
+	* Creating the sliders and placing them.
+	*/
+	public LiveColor()
+	{
+		super("Changing colors"); // setting title
+
+		// creating sliders. rgb 0-255
+		redSlider = new JSlider(JSlider.VERTICAL, 0, 255, 0);
+		greenSlider = new JSlider(JSlider.VERTICAL, 0, 255, 0);
+		blueSlider = new JSlider(JSlider.VERTICAL, 0, 255, 0);
+
+
+		// creating label table
+		Hashtable<Integer, JLabel> labelTable = new Hashtable<>();
+		for (int i = 0; i < 250; i += 20) labelTable.put(i, new JLabel("" + i));
+		labelTable.put(255, new JLabel("" + 255));
+
+		// setting labels
+		redSlider.setLabelTable(labelTable);
+		greenSlider.setLabelTable(labelTable);
+		blueSlider.setLabelTable(labelTable);
+		redSlider.setPaintLabels(true);
+		greenSlider.setPaintLabels(true);
+		blueSlider.setPaintLabels(true);
+
+		// creating the slider and label panels
+		JPanel centerPanel = new JPanel();
+		JPanel southPanel = new JPanel();
+
+		// setting layout
+		centerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 60, 0));
+		southPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 60, 0));
+
+
+		// adding sliders
+		centerPanel.add(redSlider);
+		centerPanel.add(greenSlider);
+		centerPanel.add(blueSlider);
+		add(centerPanel);
+
+		// creating labels
+		Font font = new Font("Serif", Font.BOLD, 20);
+		redLabel = new JLabel("Red");
+		redLabel.setForeground(Color.RED);
+		redLabel.setFont(font);
+		greenLabel = new JLabel("Green");
+		greenLabel.setForeground(Color.GREEN);
+		greenLabel.setFont(font);
+		blueLabel = new JLabel("Blue");
+		blueLabel.setForeground(Color.BLUE);
+		blueLabel.setFont(font);
+
+		// adding labels
+		southPanel.add(redLabel);
+		southPanel.add(greenLabel);
+		southPanel.add(blueLabel);
+		add(southPanel, BorderLayout.SOUTH);
+
+		// initializing the serial port
+		initSerialPort();
+
+		// adding handlers
+		redSlider.addChangeListener(this);
+		greenSlider.addChangeListener(this);
+		blueSlider.addChangeListener(this);
+	}
+
+	/**
+	* The handler for the sliders.
+	* @param event The event that is being fired
+	*/
+	@Override
+	public void stateChanged(ChangeEvent event)
+	{
+		String serialData = "R" + redSlider.getValue() + "G" + greenSlider.getValue() + "B" + blueSlider.getValue();
+		try
+		{
+			port.write(serialData.getBytes());
+		}
+		catch (IOException ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	/**
+	* Creating the serial communication channel.
+	*/
+	public void initSerialPort()
+	{
+		try
+		{
+			CommPortIdentifier portId = CommPortIdentifier.getPortIdentifier("COM3"); // /dev/ttyUSB0
+			SerialPort serialPort = (SerialPort) portId.open("Live Color", 5000);
+			int baudRate = 9600;
+			serialPort.setSerialPortParams(baudRate, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+			serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
+			port = serialPort.getOutputStream();
+		}
+		catch (Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	/**
+	* The main method creates the frame.
+	* @param args The arguments from the cmd. Not used in this program.
+	*/
+	public static void main(String[] args)
+	{
+		LiveColor frame = new LiveColor();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(350, 270);
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
+}
