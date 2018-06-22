bb81e1c92d9b4e3a9b3ab8675a3e6f23c8d15ea8
YardenShoham <hrsi88@gmail.com>
2018-06-22 18:59:38 +0300

    Fixed typo.

--- Src/LiveColor.java
+++ Src/LiveColor.java
@@ -96,7 +96,7 @@ public class LiveColor extends JFrame implements ChangeListener
 		try
 		{
 			port.write(serialData.getBytes());
-			Thread.sleep(150); // so the arduino wouuldn't receive so much data in such little time
+			Thread.sleep(150); // so the arduino wouldn't receive so much data in such little time
 		}
 		catch (Exception ex)
 		{
